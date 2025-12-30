9798cf21763526101d004b62caf3784231a7b963
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-12-31 01:57:20 +0300

    user login and register functionality & auth json-webtoken and cookie & user information store with zustand & verify json-webtoken

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,13 @@ import boardRouter from "./routes/board.route.js";
 
 import connectDB from "./utils/connectDB.js";
 
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
 
 app.use("/users", userRouter);
 app.use("/pins", pinRouter);
